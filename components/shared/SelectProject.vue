a537d3eaaaeaebd6df75c7c3d2e30fea967e48b0
Thanawat Settapong <thanawat.se@sansiri.com>
2024-10-17 16:40:08 +0700

    poc: autocomplete search project but still issue lag when more data

--- components/shared/SelectProject.vue
+++ components/shared/SelectProject.vue
@@ -1,9 +1,12 @@
 <template>
   <USelect v-model="selectedProject" :options="list.map((d) => ({
-    value: d.projectCode,
-    label: `${d.projectCode} - ${d.projectName}`
+    value: d.id,
+    label: d.name
   }))" :placeholder="props.placeholder || 'เลือกโครงการ'" @change="$emit('select', selectedProject)"
     :loading="loading" />
+  <!-- <UInputMenu class="w-[500px]" :placeholder="props.placeholder || 'เลือกโครงการ'" v-model="selectedProject"
+    :options="list" value-attribute="id" option-attribute="name" :search-attributes="['id', 'name']" :loading="loading"
+    @change="$emit('select', selectedProject)"  /> -->
 </template>
 
 <script lang="ts" setup>
@@ -17,8 +20,8 @@ const userStore = useUserStore()
 const selectedProject = ref<string>('')
 const loading = ref(false)
 const list = ref([] as {
-  projectCode: string,
-  projectName: string
+  id: string,
+  name: string
 }[])
 
 const resetSelection = () => {
@@ -30,7 +33,7 @@ const fetchData = async () => {
   loading.value = true;
 
   const [projects] = await Promise.all([useMasterDataApi(userStore.token!).getProjectList()])
-  list.value = projects;
+  list.value = projects.map((p: any) => ({ id: p.projectCode, name: `${p.projectCode} - ${p.projectName}` }));
 
   loading.value = false;
 }
